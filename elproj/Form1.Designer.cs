fe891bbcba5c50d58686ac74b342e5179c77b7ec
Abdelrahman Elsharka <Abdelrahman Elsharka@SHARKAWY>
2024-08-04 21:45:24 +0300

    add

--- elproj/Form1.Designer.cs
+++ elproj/Form1.Designer.cs
@@ -38,10 +38,10 @@
             // 
             button1.BackgroundImage = Properties.Resources.click;
             button1.BackgroundImageLayout = ImageLayout.Stretch;
-            button1.Location = new Point(195, 230);
-            button1.Margin = new Padding(2);
+            button1.Location = new Point(362, 433);
+            button1.Margin = new Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new Size(81, 69);
+            button1.Size = new Size(150, 130);
             button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
@@ -49,43 +49,45 @@
             // label1
             // 
             label1.Location = new Point(0, 0);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(100, 23);
+            label1.Size = new Size(186, 43);
             label1.TabIndex = 3;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(181, 182);
-            textBox1.Margin = new Padding(2);
+            textBox1.Location = new Point(336, 343);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(95, 25);
+            textBox1.Size = new Size(173, 39);
             textBox1.TabIndex = 2;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(159, 163);
+            label2.Location = new Point(295, 307);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(133, 17);
+            label2.Size = new Size(241, 32);
             label2.TabIndex = 4;
             label2.Text = "please enter your job";
             label2.Click += label2_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.hospital_building_for_healthcare_background_illustration_with_ambulance_car_doctor_patient_nurses_and_medical_clinic_exterior_free_vector;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(463, 275);
+            ClientSize = new Size(860, 518);
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
-            Margin = new Padding(2);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Main Menu";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
