0182d54d0c3c2d15a91ef7c4501d97c1d7b501be
hazem mohab <hazemmohabmaher@gmail.com>
2024-08-04 20:11:53 +0300

    ha

--- elproj/Form3.Designer.cs
+++ elproj/Form3.Designer.cs
@@ -42,6 +42,7 @@
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(134, 25);
             comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // button1
             // 
